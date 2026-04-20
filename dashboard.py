19a153b7d9e7bf7547788594604bfef868893c9a
Mohammad Mahdi Shokor <mohammadmahdishokor@gmail.com>
2026-04-20 12:01:10 +0300

    added sidebar+ fixed some issues

--- dashboard.py
+++ dashboard.py
@@ -8,17 +8,17 @@ from info import affectedTargetsTypes , incidentTypes,affectedTargetsTypes,ratin
 def dashboardPage():
     start = datetime(2025, 10, 27)
     end = datetime.now()
-    tempRating =round(np.random.uniform(0, 10), 1)
-    reports = [{"name":"guest",
+    reports = [] 
+    for _ in range(100):
+        tempRating =round(np.random.uniform(0, 10), 1)
+        reports.append ({"name":"guest",
                 "title":"",
                 "type":incidentTypes[0],
                 "rating":tempRating,
                 "severity":ratingToSeverity(tempRating),
                 "description":"",
                 "time":start + timedelta( seconds=np.random.randint(0, int((end - start).total_seconds())))
-                }] #first one is sample so I understand how the list of maps would look like
-    
-    
+        })
     st.write (reports)
 
  
